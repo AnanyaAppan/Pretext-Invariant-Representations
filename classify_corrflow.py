4b050c89ebc69e9ab91ac6d3962dda62f6ce9a35
Ananya <ananya.appan@gmail.com>
2021-02-25 15:20:28 +0530

    changed to state dict while loading model

--- classify_corrflow.py
+++ classify_corrflow.py
@@ -60,7 +60,7 @@ class ClassifyLSTM(nn.Module):
         rnn_num_layers = 1
         
         self.baseModel = CorrFlow().to(device)
-        self.baseModel.load_state_dict(torch.load("../weights/corrflow.pth"))
+        self.baseModel.load_state_dict(torch.load("../weights/corrflow.pth")['state_dict'])
         self.dropout = nn.Dropout(dr_rate)
         self.lstm_layer = nn.LSTM(128, 256, 10)
         self.fc1 = nn.Linear(256,128)
